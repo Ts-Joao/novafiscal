65c5915fb89c892959d69a639f1b6da054c73a27
Ts-Joao <cttjoaovts@gmail.com>
2026-07-02 15:34:14 -0300

    feat: implementa novas alteracoes ao controller

--- backend/src/main/java/com/novafiscal/backend/purchase/api/controller/PurchaseController.java
+++ backend/src/main/java/com/novafiscal/backend/purchase/api/controller/PurchaseController.java
@@ -1,14 +1,22 @@
 package com.novafiscal.backend.purchase.api.controller;
 
+import com.novafiscal.backend.common.response.ApiResponse;
 import com.novafiscal.backend.purchase.api.dto.PurchaseRequestDTO;
-import com.novafiscal.backend.purchase.domain.service.PurchaseService;
+import com.novafiscal.backend.purchase.api.dto.PurchaseResponseDTO;
+import com.novafiscal.backend.purchase.application.PurchaseService;
+import com.novafiscal.backend.purchase.domain.model.Purchase;
+import com.novafiscal.backend.purchase.mapper.PurchaseMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,12 +24,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class PurchaseController {
 
     private final PurchaseService purchaseService;
+    private final PurchaseMapper purchaseMapper;
 
     @PostMapping
-    public ResponseEntity<String> createPurchase(
-            @Valid @RequestBody PurchaseRequestDTO dto
-            ) {
-        String id = purchaseService.createPurchase(dto);
-        return ResponseEntity.ok(id);
+    public ResponseEntity<ApiResponse<PurchaseResponseDTO>> createPurchase(
+            @Valid @RequestBody PurchaseRequestDTO dto) {
+
+        Purchase purchase = purchaseMapper.toDomain(dto);
+        Purchase created = purchaseService.create(purchase);
+        PurchaseResponseDTO response = purchaseMapper.toResponse(created);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ApiResponse<>(Instant.now(), response));
     }
 }
