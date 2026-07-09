f0f623b5d738f3b7a3e1d2dd040e7052e3e5a951
Ts-Joao <cttjoaovts@gmail.com>
2026-07-09 10:34:40 -0300

    feat: implementa testes de dominio de Address

---
+++ backend/src/test/java/com/novafiscal/backend/customer/domain/AddressTest.java
@@ -0,0 +1,90 @@
+package com.novafiscal.backend.customer.domain;
+
+import org.junit.jupiter.api.Test;
+
+import com.novafiscal.backend.customer.domain.model.Address;
+import com.novafiscal.backend.customer.domain.model.AddressType;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+
+public class AddressTest {
+
+    @Test
+    void shouldCreateAddress_withValidData() {
+        Address address = Address.create(
+            AddressType.BILLING,
+            "Rua João da Silva",
+            "123",
+            "casa",
+            "Centro",
+            "Blumenau",
+            "SC",
+            "12345-123",
+            true);
+        assertNotNull(address);
+        assertEquals(AddressType.BILLING, address.getType());
+        assertEquals("Rua João da Silva", address.getStreet());
+        assertEquals("123", address.getNumber());
+        assertEquals("Centro", address.getNeighborhood());
+        assertEquals("12345-123", address.getZipCode());
+        assertEquals("SC", address.getState());
+        assertEquals("Blumenau", address.getCity());
+        assertTrue(address.isDefault());
+    }
+
+    @Test
+    void shouldReconstituteAddress_withValidData() {
+        Address address = Address.reconstitute(
+            UUID.randomUUID(),
+            AddressType.BILLING,
+            "Rua João da Silva",
+            "123",
+            "casa",
+            "Centro",
+            "Blumenau",
+            "SC",
+            "12345-123",
+            true);
+        assertNotNull(address);
+        assertEquals(AddressType.BILLING, address.getType());
+        assertEquals("Rua João da Silva", address.getStreet());
+        assertEquals("123", address.getNumber());
+        assertEquals("Centro", address.getNeighborhood());
+        assertEquals("12345-123", address.getZipCode());
+        assertEquals("SC", address.getState());
+        assertEquals("Blumenau", address.getCity());
+        assertTrue(address.isDefault());
+    }
+
+    @Test
+    void shouldUpdateAddress_withValidData() {
+        Address address = Address.create(
+            AddressType.BILLING,
+            "Rua João da Silva",
+            "123",
+            "casa",
+            "Centro",
+            "Blumenau",
+            "SC",
+            "12345-123",
+            true);
+        address.update(
+            "Rua João da Silva",
+            "123",
+            "casa",
+            "Centro",
+            "Blumenau",
+            "SC",
+            "12345-123");
+        assertNotNull(address);
+        assertEquals(AddressType.BILLING, address.getType());
+        assertEquals("Rua João da Silva", address.getStreet());
+        assertEquals("123", address.getNumber());
+        assertEquals("Centro", address.getNeighborhood());
+        assertEquals("12345-123", address.getZipCode());
+        assertEquals("SC", address.getState());
+        assertEquals("Blumenau", address.getCity());
+    }
+}
